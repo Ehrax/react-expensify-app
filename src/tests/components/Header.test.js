4278c30ecda24a8a0fe2e36e009b3136d8af87ba
Alekei <alexander.rasputin@uni-ulm.de>
2017-11-16 15:12:55 +0100

    Add private Routes

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,14 +2,9 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
 
-import Header from '../../components/Header'
+import { Header } from '../../components/Header'
 
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />)
+  const wrapper = shallow(<Header startLogout={() => { }} />)
   expect(toJson(wrapper)).toMatchSnapshot()
-
-  // expect(wrapper.find('h1').text()).toBe('Expensify')
-  // const renderer = new ReactShallowRenderer()
-  // renderer.render(<Header />)
-  // expect(renderer.getRenderOutput()).toMatchSnapshot()
 })
